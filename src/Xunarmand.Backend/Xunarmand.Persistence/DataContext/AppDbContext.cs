7a0b184b5046bf2df6b3972a276f28e66c773aae
Husanboy <samadovxusan9013@gmail.com>
2024-04-14 17:57:54 +0500

    Add host configuration and appdbcontext

---
+++ src/Xunarmand.Backend/Xunarmand.Persistence/DataContext/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Xunarmand.Persistence.DataContext;
+
+public class AppDbContext:Microsoft.EntityFrameworkCore.DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
