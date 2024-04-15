78497129ea90bbcc40bffa3442384a13f9db53a6
Husanboy <samadovxusan9013@gmail.com>
2024-04-15 12:21:42 +0500

    Add user enity and configuration

---
+++ src/Xunarmand.Backend/Xunarmand.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using Xunarmand.Domain.Common.Entities;
+
+namespace Xunarmand.Domain.Entities;
+
+public class User:AuditableEntity
+{
+    public Guid Id { get; set; }
+    
+    public string? FirstName { get; set; }
+    
+    public string? LastName { get; set; }
+
+    public string Email { get; set; } = default!;
+
+    public string Password { get; set; } = default!;
+    
+    public string? PhoneNumber { get; set; }
+    
+    public string? Brithday{ get; set; }
+}
---
+++ src/Xunarmand.Backend/Xunarmand.Persistence/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Xunarmand.Domain.Entities;
+
+namespace Xunarmand.Persistence.EntityConfigurations;
+
+public class UserConfiguration:IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(client => client.FirstName).HasMaxLength(64).IsRequired();
+        builder.Property(client => client.LastName).HasMaxLength(64).IsRequired();
+        builder.Property(client => client.Email).HasMaxLength(128).IsRequired();
+        builder.Property(client => client.Password).HasMaxLength(128).IsRequired();
+        
+    }
+}
