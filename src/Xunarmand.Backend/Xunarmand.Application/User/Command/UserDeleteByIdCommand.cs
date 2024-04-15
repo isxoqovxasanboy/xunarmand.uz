413c281ac7cb85e1e70fac059c8f3717e6354ad4
Husanboy <samadovxusan9013@gmail.com>
2024-04-15 15:55:49 +0500

    Add Commands and CommandsHandlers

---
+++ src/Xunarmand.Backend/Xunarmand.Application/User/Command/UserDeleteByIdCommand.cs
@@ -0,0 +1,8 @@
+﻿using Xunarmand.Domain.Common.Commands;
+
+namespace Xunarmand.Application.User.Command;
+
+public record UserDeleteByIdCommand:ICommand<bool>
+{
+    public Guid UserId { get; set; }
+}
