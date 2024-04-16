a6f12d1524e90e96b0362f5e9f10f3249937c41b
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-04-16 22:46:43 +0500

    Fixed seed date

--- src/Xunarmand.Backend/Xunarmand.Api/Data/SeedDataExtentions.cs
+++ src/Xunarmand.Backend/Xunarmand.Api/Data/SeedDataExtentions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Globalization;
+using Microsoft.EntityFrameworkCore;
 using Xunarmand.Domain.Entities;
 using Xunarmand.Persistence.DataContext;
 
@@ -26,7 +27,7 @@ public static class  SeedDataExtensions
                     Id = Guid.Parse("54e16318-d140-4453-80c9-1a117dbe75fd"),
                     FirstName = "John",
                     LastName = "Doe",
-                    Birthday = DateOnly.Parse("10/20/2002"),
+                    Birthday = DateOnly.Parse("10.20.2002",CultureInfo.InvariantCulture),
                     PhoneNumber = "123456789012",
                     EmailAddress = "example@gmail.com",
                     PasswordHash = "$2a$12$pHdneNbJGp4SnN1ovHrNqevf6I.k3Gy.7OMJoWWB0RByv0foi4fgy", // qwerty123
@@ -36,7 +37,7 @@ public static class  SeedDataExtensions
                     Id = Guid.Parse("5edbb0fe-7263-4f75-bad8-c9f3d422dd1d"),
                     FirstName = "Bob",
                     LastName = "Richard",
-                    Birthday = DateOnly.Parse("10.20.2000"),
+                    Birthday = DateOnly.Parse("10.20.2000",CultureInfo.InvariantCulture),
                     PhoneNumber = "123456789012",
                     EmailAddress = "tastBobRichard@gmail.com",
                     PasswordHash = "$2a$12$LxSqe5AE7AtglesHHK5NROFdJQdA1r1XKqhzg4q/tMTZjVEH0PNSK", //asdf1234
@@ -46,7 +47,7 @@ public static class  SeedDataExtensions
                     Id = Guid.Parse("6357D344-CB69-4FAA-81C5-AC0FC59AE0F9"),
                     FirstName = "Sarah",
                     LastName = "Funk",
-                    Birthday = DateOnly.Parse("10.10.2002"),
+                    Birthday = DateOnly.Parse("10.10.2002",CultureInfo.InvariantCulture),
                     PhoneNumber = "123456789012",
                     EmailAddress = "sarah.funk@gmail.com",
                     PasswordHash = "$2a$12$LxSqe5AE7AtglesHHK5NROFdJQdA1r1XKqhzg4q/tMTZjVEH0PNSK", //asdf1234
