0b5ad4609bda4a35a5cafc57c794e349c7d60ad2
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-04-17 14:06:27 +0500

    Add order entity

---
+++ src/Xunarmand.Backend/Xunarmand.Domain/Entities/Order.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using Xunarmand.Domain.Common.Entities;
+using Xunarmand.Domain.Enums;
+
+namespace Xunarmand.Domain.Entities;
+
+/// <summary>
+/// Represents an order for a product.
+/// </summary>
+public class Order : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the price of the product in the order.
+    /// </summary>
+    public decimal Price { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the amount of the product in the order.
+    /// </summary>
+    public int ProductAmount { get; set; }
+
+    /// <summary>
+    /// Gets or sets the ID of the product associated with the order.
+    /// </summary>
+    public Guid ProductId { get; set; } = default!;
+    
+    /// <summary>
+    /// Gets or sets the product associated with the order.
+    /// </summary>
+    public Product Product { get; set; } 
+}
---
+++ src/Xunarmand.Backend/Xunarmand.Persistence/EntityConfigurations/OrderConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Xunarmand.Domain.Entities;
+
+namespace Xunarmand.Persistence.EntityConfigurations;
+
+public class OrderConfiguration: IEntityTypeConfiguration<Order> 
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.Property(order => order.Price).HasColumnType("decimal(18,2)").IsRequired();
+        builder.Property(order => order.ProductAmount).HasColumnType("decimal(18,2)").IsRequired();
+
+        builder
+            .HasOne(order => order.Product)
+            .WithMany(product => product.Orders)
+            .HasForeignKey(order => order.ProductId);
+    }
+}
