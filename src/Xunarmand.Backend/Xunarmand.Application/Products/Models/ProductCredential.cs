0a5d7b0b32a8a500e984791290a4fbd590b1ab8d
Husanboy <samadovxusan9013@gmail.com>
2024-04-18 13:01:27 +0500

    Add product models

---
+++ src/Xunarmand.Backend/Xunarmand.Application/Products/Models/ProductCredential.cs
@@ -0,0 +1,26 @@
+﻿namespace Xunarmand.Application.Product.Models;
+
+public class ProductCredential
+{
+    public string? ProductName { get;  set; }
+
+    /// <summary>
+    /// Gets or sets the type of the product.
+    /// </summary>
+    public string? Type { get;  set; }
+
+    /// <summary>
+    /// Gets or sets the description of the product.
+    /// </summary>
+    public string? Description { get;  set; }
+
+    /// <summary>
+    /// Gets or sets the price of the product.
+    /// </summary>
+    public decimal? Price { get;  set; }
+
+    /// <summary>
+    /// Gets or sets the URL of the product image.
+    /// </summary>
+    public string? ImageUrl { get; set; }
+}
